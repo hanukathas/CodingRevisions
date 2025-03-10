d546f7001b1f3297052117c18cc30d0cda4dcaca
Hanu Kathas <hanumankathas@gmail.com>
2025-03-10 15:33:10 -0700

    practice_problems_ik

---
+++ revisions/practice_problems_ik/minimum_coins.py
@@ -0,0 +1,10 @@
+def minimum_coins(coins, value):
+    dp = [float('inf')] * (value+1)
+    dp[0] = 0
+    for i in range(value+1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i-coin]+1)
+    return dp[value] if dp[value] != float('inf') else -1
+if __name__ == '__main__':
+    print(minimum_coins([2,3,5], 1))
