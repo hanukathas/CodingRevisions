21ea1aaa71642c5c3760abffba1597c035485fcc
Hanu Kathas <hanumankathas@gmail.com>
2025-04-17 21:56:34 -0700

    trees

---
+++ leetcode/roblox/reorganize_string.py
@@ -0,0 +1,17 @@
+from collections import Counter, deque
+
+
+def re_organize_string(s: str):
+    s = sorted(s)
+
+    # base cases:
+
+
+if __name__ == '__main__':
+    print(re_organize_string("aab"))
+    print(re_organize_string("aaabcd"))
+    print(re_organize_string("baaba"))
+    print(re_organize_string("aaabbccdddd"))
+    a = [1,2,3,4,5]
+
+
