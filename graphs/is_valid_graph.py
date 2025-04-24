216ba414d8368687d5d1cdae30bfa5a136b7b752
Hanu Kathas <hanumankathas@gmail.com>
2025-04-24 07:16:47 -0700

    4_23

---
+++ graphs/is_valid_graph.py
@@ -0,0 +1,9 @@
+def is_valid_graph(n: int, edges: list):
+    """
+    1. component count = 1. A single queue loop visits all vertexes
+    2. at least one vertex has odd edges
+    
+    :param n:
+    :param edges:
+    :return:
+    """
