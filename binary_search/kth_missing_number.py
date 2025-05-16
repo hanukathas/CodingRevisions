81b47ab641a0e5429d1e77022e2744487f45ce06
Hanu Kathas <hanumankathas@gmail.com>
2025-05-16 08:06:46 -0700

    binary_search_5_15

--- binary_search/kth_missing_number.py
+++ binary_search/kth_missing_number.py
@@ -5,17 +5,35 @@ def kth_missing_number(nums: list, k: int):
     missing = 0
     while start <= end:
         mid = start + (end - start) // 2
-        missing = nums[mid] - nums[0] - mid
+        missing = nums[mid] - (nums[0] + mid)
         if missing < k:
             start = mid + 1
         else:
             end = mid - 1
 
-    print(start, end, missing)
-    missing = nums[end] - nums[0] - end
-    print(missing)
+    # print(start, end, missing)
+    missing = nums[end] - (nums[0] + end)
+    # print(missing)
     return nums[end] + k - missing
+
+def kth_missing_number_r(nums: list, k: int):
+    start = 0
+    n = len(nums)
+    end = n - 1
+    missing = 0 # after calculating mid, get how many values are missing
+    # missing = nums[mid] - (nums[start] + mid)
+    while start <= end:
+        mid = start + (end - start) // 2
+        missing = nums[mid] - (nums[0] + mid)
+        if missing < k:
+            start = mid + 1
+        else:
+            end = mid - 1
+    missing = nums[end] - (nums[0] + end)
+    return nums[end] + k - missing
+
 
 if __name__ == '__main__':
     # print(kth_missing_number([4, 7, 9, 10], 1))
     print(kth_missing_number([4, 7, 9, 10], 3))
+    print(kth_missing_number_r([4, 7, 9, 10], 3))
--- binary_search/search_insert_position.py
+++ binary_search/search_insert_position.py
@@ -10,8 +10,22 @@ def search_insert_position(arr: list, n: int):
             end = mid - 1
         if arr[mid] < n:
             start = mid + 1
+        print(start, end, mid)
+    return start
+
+def search_insert_position_r(arr: list, n: int):
+    start = 0
+    end = len(arr) - 1
+    while start <= end:
+        mid = start + (end - start) // 2
+        if arr[mid] == n:
+            return mid
+        elif arr[mid] < n:
+            start = mid + 1
+        else:
+            end = mid - 1
     return start
 
 if __name__ == '__main__':
-    print(search_insert_position([1,4,5,7,9], 7))
+    print(search_insert_position([1,4,5,7,9], 3))
     print(search_insert_position([1, 4, 5, 9], 7))
